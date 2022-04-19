7896eaf9944e7bec343073b09733aacbd23cfb96
mr-Xristos <100307436+mr-Xristos@users.noreply.github.com>
2022-04-19 21:07:14 +0300

    Delete creds.py

--- creds.py
+++
@@ -1,5 +0,0 @@
-host = 'ec2-54-157-79-121.compute-1.amazonaws.com'
-database = 'd9ktlaq31r9jaf'
-user = 'jvpivoxxexdibw'
-port = 5432
-password = 'e37c1f653326b8281041e2aa4d6189e9a437e5fa3e87384ca67cfc82f0102f3c'
